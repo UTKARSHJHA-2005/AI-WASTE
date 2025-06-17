8d5433b37e3b4f445370dfe27779f07f412c6eb1
Daya Sagar <152199702+Dayasagar88@users.noreply.github.com>
2025-06-17 11:50:58 +0530

    mobile menu bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,17 +16,37 @@ const Navbar = () => {
       {/* Navbar */}
       <nav className="flex justify-between items-center py-4 px-8 md:px-16 bg-white/80 backdrop-blur-sm shadow-sm relative">
         {/* Logo */}
-        <div onClick={() => navigate("/")} className="flex hover:scale-110 transition-transform items-center cursor-pointer">
-          <img src={ecoLogo} alt="EcoGenie Logo" className="h-[30px] w-[30px]" />
-          <p className="font-serif text-xl font-bold text-green-700 ml-2">ECOGENIE</p>
+        <div
+          onClick={() => navigate("/")}
+          className="flex hover:scale-110 transition-transform items-center cursor-pointer"
+        >
+          <img
+            src={ecoLogo}
+            alt="EcoGenie Logo"
+            className="h-[30px] w-[30px]"
+          />
+          <p className="font-serif text-xl font-bold text-green-700 ml-2">
+            ECOGENIE
+          </p>
         </div>
         {/* Centered Menu */}
         <div className="hidden md:flex space-x-8 absolute left-1/2 transform -translate-x-1/2">
-          {[{ name: "Dashboard", href: "/dashboard" }, { name: "Product", href: "/products" }, { name: "Processors", href: "/recycler" }].map((item, index) => (
-            <a key={index} href={item.href} className={`font-medium transition-colors ${location.pathname === item.href ? "text-green-700 font-semibold" : "text-gray-800 hover:text-green-600"
-              }`}>
+          {[
+            { name: "Dashboard", href: "/dashboard" },
+            { name: "Product", href: "/products" },
+            { name: "Processors", href: "/recycler" },
+          ].map((item, index) => (
+            <Link
+              key={index}
+              to={item.href}
+              className={`font-medium transition-colors ${
+                location.pathname === item.href
+                  ? "text-green-700 font-semibold"
+                  : "text-gray-800 hover:text-green-600"
+              }`}
+            >
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
         {/* User Icon */}
@@ -34,7 +54,7 @@ const Navbar = () => {
           <div className="flex items-center cursor-pointer hover:scale-110 transition-transform border border-green-400 rounded-full p-2 shadow-inner shadow-green-500 md:flex">
             <FaUser className="text-2xl" />
           </div>
-         </Link>
+        </Link>
         {/* Mobile Menu Button */}
         <div className="md:hidden">
           <button onClick={toggleMenu} className="text-green-700 text-2xl">
@@ -45,15 +65,26 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       {menuOpen && (
- <div className="md:hidden flex flex-col items-center bg-white shadow-md p-4 space-y-4">
-    {[{ name: "Dashboard", href: "/dashboard" }, { name: "Product", href: "/products" }, { name: "Processors", href: "/recycler" }].map((item, index) => (
-      <Link  key={index} to={item.href} className={`font-medium transition-colors ${location.pathname === item.href ? "text-green-700 font-semibold" : "text-gray-800 hover:text-green-600"
-          }`}onClick={() => setMenuOpen(false)}>
-        {item.name}
-      </Link>
-    ))}
-  </div>
-)}
+        <div className="md:hidden flex flex-col items-center bg-white shadow-md p-4 space-y-4">
+          {[
+            { name: "Dashboard", href: "/dashboard" },
+            { name: "Product", href: "/products" },
+            { name: "Processors", href: "/recycler" },
+          ].map((item, index) => (
+            <Link
+              key={index}
+              to={item.href}
+              className={`font-medium transition-colors  z-50 ${
+                location.pathname === item.href
+                  ? "text-green-700 font-semibold"
+                  : "text-gray-800 hover:text-green-600"
+              }`}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
